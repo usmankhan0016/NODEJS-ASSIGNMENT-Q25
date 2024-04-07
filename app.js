dd361d0fc31b4d916811649c2518ce38b6619ae8
Usman Khan <160907897+usmankhan0016@users.noreply.github.com>
2024-04-07 14:04:32 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+// Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+let alien_color = "green";
+// • Write an if statement to test whether the alien’s color is green.
+if (alien_color === "green") {
+    console.log("player just earned 5 points!");
+}
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("player just earned 5 points!");
+}
